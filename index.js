a4563b511bf871d2f274945be2066fd3f3b67c37
mustabur <mustaburrahman2017@hotmail.com>
2020-11-07 08:26:05 +0000

    function created for technical test

---
+++ index.js
@@ -0,0 +1,40 @@
+const arr = [
+    // ["cassettes", 4],
+    [5, 10000],
+    [10, 20000],
+    [20, 40000],
+    [501, 100000],
+  ];
+  
+  function customerOrder(arr) {
+    let cassettes;
+    if (arr[0][0] === "cassettes") {
+      cassettes = arr[0][1];
+    } else {
+      cassettes = 4;
+    }
+  
+    let order = [];
+  
+    for (let i = 0; i <= cassettes; i++) {
+        if(arr[i][0] !== 5 && arr[i][0] !== 10 && arr[i][0] !== 20 && arr[i][0] !== 50) {
+          console.log(arr[i][0])
+          return 'Please input a valid note';
+      }
+      if (arr[i][1] / arr[i][0] > 2000) {
+        return "The total value is too high requested exceeds capacity of 2000 notes";
+      }
+      if (arr[i][1] / arr[i][0] < 2000) {
+        return "The the total value is too low, a cassesste needs to filled to 2000 notes";
+      }
+      if (arr[i][1] / arr[i][0] === 2000) {
+        order.push(arr[i]);
+      }
+      if (order.length === cassettes) {
+        return "Order is valid";
+      }
+    }
+  }
+  
+  console.log(customerOrder(arr));
+  
